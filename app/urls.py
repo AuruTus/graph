cf439690fb246d0b6e4eafbe5fafbe68ecded532
sergeysynergy <sergeysynergy@gmail.com>
2015-06-26 15:16:50 +0300

    first real commit

---
+++ app/urls.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, url
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^zcore/', include('zcore.urls', namespace='zcore')),
+    url(r'^syscheck/', include('syscheck.urls', namespace='syscheck')),
+]
+
+urlpatterns += [
+    url(r'^$', 'zcore.views.index', name='index'),
+    url(r'^force/(?P<id>[-\w]+)/$', 'zcore.views.view_force', name='viewForce'),
+    url(r'^chord/(?P<id>[-\w]+)/$', 'zcore.views.view_chord', name='viewChord'),
+
+    url(r'^create-project/$', 'zcore.views.create_project'),
+
+    url(r'^json-force/(?P<id>[-\w]+)/(?P<attributesFilter>.*)/$', 'zcore.views.json_force', name='jsonForce'),
+    url(r'^json-attributes/$', 'zcore.views.json_attributes', name='jsonAttributes'),
+
+    #url(r'^json-chord/(?P<id>[-\w]+)/rs/(?P<removeStandalone>[-\w]+)/filter/(?P<attributesFilter>[-\w]+)/$', 'zcore.views.json_chord', name='jsonChord'),
+]
+
+
