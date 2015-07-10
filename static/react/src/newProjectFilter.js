f65bfc1664b278088b24775bf15587bc78f1ff1f
sergeysynergy <sergeysynergy@gmail.com>
2015-07-10 18:37:07 +0300

    0710

---
+++ static/react/src/newProjectFilter.js
@@ -0,0 +1,176 @@
+var ATTRIBUTES = [
+    {
+        "display": "\u041d\u0430\u0438\u043c\u0435\u043d\u043e\u0432\u0430\u043d\u0438\u0435 \u0434\u043e\u043a\u0443\u043c\u0435\u043d\u0442\u0430",
+        "id": 1,
+        "name": "doc_name"
+    },
+    {
+        "display": "\u0414\u0430\u0442\u0430 \u0434\u043e\u043a\u0443\u043c\u0435\u043d\u0442\u0430",
+        "id": 2,
+        "name": "doc_timestamp"
+    },
+    {
+        "display": "\u0424\u0430\u043c\u0438\u043b\u0438\u044f",
+        "id": 3,
+        "name": "last_name"
+    },
+    {
+        "display": "\u0418\u043c\u044f",
+        "id": 4,
+        "name": "first_name"
+    },
+    {
+        "display": "\u041e\u0442\u0447\u0435\u0441\u0442\u0432\u043e",
+        "id": 5,
+        "name": "middle_name"
+    },
+    {
+        "display": "\u041c\u043e\u043c\u0435\u043d\u0442",
+        "id": 6,
+        "name": "timestamp"
+    },
+    {
+        "display": "\u041f\u043e\u043b\u043d\u043e\u0435 \u043d\u0430\u0438\u043c\u0435\u043d\u043e\u0432\u0430\u043d\u0438\u0435",
+        "id": 7,
+        "name": "full_name"
+    }
+]
+ 
+var NewProjectFilter = React.createClass({
+    getInitialState: function() {
+        return {
+            filterAttributes: ['doc_name', 'doc_timestamp'],
+            filterOptions: {zero: 'yes'},
+        }
+    },
+    componentDidMount: function() {
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        
+        // Формируем массив json-данных graphFilter
+        var graphFilter = { 
+            filterAttributes: this.state.filterAttributes ,
+            filterOptions: this.state.filterOptions
+        } 
+
+        // Преобразовываем массив json-данных graphFilter для передачи через url 
+        graphFilter = encodeURIComponent(JSON.stringify(graphFilter))
+
+        // Формируем и отправляен get-запрос на сервер
+        var client = new XMLHttpRequest()
+        var url = '/create-project/' + graphFilter
+        client.open('GET', url)
+        client.send()
+    },
+    render: function() {
+        return (
+            <form onSubmit={this.handleSubmit} ref="forceGraphFilterForm">
+                <AttributesList 
+                    attributes={ATTRIBUTES} 
+                    filterAttributes={this.state.filterAttributes}
+                />
+                <input type="submit" className="btn btn-warning" value="Filter" />
+            </form>
+        );
+    },
+    graphUpdate: function() {
+        // Формируем массив json-данных graphFilter
+        var graphFilter = { 
+            filterAttributes: this.state.filterAttributes ,
+            filterOptions: this.state.filterOptions
+        } 
+    },
+});
+
+var AttributesList = React.createClass({
+    getInitialState: function() {
+        // Задаём начальную инициализацию фильтра для выбранных атрибутов
+        return {
+            initValues: this.props.filterAttributes
+        }
+    },
+    componentDidMount: function() {
+        //console.log(this.props.filterAttributes)
+    },
+    updateFilterAttributes: function(value) {
+        console.log('value ',value)
+        console.log('filterAttributes ',this.props.filterAttributes)
+        if (value == '') {
+            this.props.filterAttributes.pop(value)
+        } else {
+            this.props.filterAttributes.push(value)
+        }
+        console.log('filterAttributes ',this.props.filterAttributes)
+
+        // Передаём обработку клика родительскому компоненту
+        if (typeof this.props.reClick === 'function') {
+            //this.props.handleClick(e.target.value);
+            //this.props.reClick();
+        }
+    },
+    render: function() {
+        var rows = []
+        this.props.attributes.forEach(function(attribute) {
+            rows.push(<AttributeCheckbox 
+                key={attribute.id} 
+                display={attribute.display} 
+                value={attribute.name} 
+                updateFilterAttributes={this.updateFilterAttributes} 
+                initValues={this.state.initValues}
+            />)
+        }.bind(this))
+        return (
+            <div className="btn-group" data-toggle="buttons">
+            {rows}
+            </div>
+        );
+    }
+});
+
+var AttributeCheckbox = React.createClass({
+    getInitialState: function() {
+        var value = ''
+        var className = "btn btn-primary"
+        if (inArray(this.props.value, this.props.initValues)) {
+            value = this.props.value
+            className = "btn btn-primary active"
+        }
+        return {
+            value: value,
+            className: className
+        }
+    },
+    componentDidMount: function() {
+        //console.log(this.props.key,this.state.value)
+    },
+    handleClick: function() {
+        var value = (this.state.value == '') ? this.props.value: ''
+        this.setState({value: value})
+
+        // Передаём обработку родительскому компоненту
+        if (typeof this.props.updateFilterAttributes === 'function') {
+            this.props.updateFilterAttributes(value)
+        }
+    },
+    render: function() {
+        return (
+            <label className={this.state.className} onClick={this.handleClick}>
+            <input
+                type="checkbox" 
+                value={this.state.value}
+                ref="filterCheckbox"
+            />
+            {this.props.display}
+            </label>
+        );
+    }
+});
+
+
+React.render(
+    <NewProjectFilter />, 
+    document.getElementById('newProjectFilter')
+);
+
+
--- zcore/models.py
+++ zcore/models.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+import json
+import networkx as nx
+from networkx.readwrite import json_graph
+#from random import randint
+import numpy as np
+#from numpy import array
+
 from django.db import models
+from django.db import connections
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 class Graph(models.Model):
@@ -22,5 +31,237 @@ class Method(models.Model):
     graph = models.ForeignKey(Graph)
     model_title = models.CharField(max_length=200, default='')
 """
+
+
+# Отформатированный вывод данных в формате json
+def print_json(data):
+    print(json.dumps(data, indent=4, sort_keys=True))
+
+
+# Вывод сформированных данных для отладочных целей 
+def render_content(content):
+    response = HttpResponse()
+    response['Content-Type'] = "text/javascript; charset=utf-8"
+    print(content)
+    response.write(content)
+    return response 
+
+
+# Получение атрибутов информационного объекта вида узел
+def get_node_attributes(element_id):
+    sql = "SELECT prpdf.name, prpdf.display, prp.str_val FROM properties as prp, propertydefs as prpdf WHERE prp.def_id=prpdf.id AND target_id=" + str(element_id)
+    cursor = connections['mysql'].cursor()
+    data = []
+
+    cursor.execute(sql)
+    attributes = cursor.fetchall()
+    for attribute in attributes:
+        data.append({'val':attribute[0],'name':attribute[1],'display':attribute[2]})
+
+    return data
+
+
+# Получение атрибутов информационного объекта вида дуга
+def get_edge_attributes(element_id):
+    return ''
+
+
+# Добавляем узел в граф при создании многомерной проекции "семантической кучи"
+def add_node_from_db(id, G, nodeData=False):
+
+    # Проверяем, передаётся ли в функцию значение поля data:
+    # eсли значение не передаётся, мы совершаем дополнительный запрос к базе данных.
+    # Это необходимо когда id узла полученно каким-то другим способом.
+    # Например, в результате запроса к таблице связей relations.
+    if not nodeData:
+        cursor = connections['mysql'].cursor()
+        sql = "SELECT el.data  FROM elements as el WHERE el.id="+str(id)
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        nodeData = row[0]
+
+    # Для каждого узла с помощью отдельной функции получаем словарь атрибутов
+    nodeAttributes = get_node_attributes(id)
+
+    # Добавляем узел в граф вместе с полученным словарём атрибутов.
+    # В качестве атрибута data указываем значение поля data, 
+    # в последствии, это значение будет использованно в поиске по-умолчанию
+    G.add_node(id, data=nodeData, attributes=nodeAttributes)
+
+    return True
+
+
+# Для тестовых целей:
+# Создание графа - многомерной проекции "семантической кучи" - первым методом
+def create_graph_method_01():
+    print('Create projection using method 01')
+    G = nx.Graph() # Cоздаём пустой NetworkX-граф
+
+    # Создаём объект типа cusros, который позволяет нам подключиться и работаться с базой данных,
+    # содержащей данные многомерной матрицы
+    cursor = connections['mysql'].cursor()
+
+    # Формируем sql-запрос к таблице elements, содержащей информационные объекты (далее ИО).
+    # Данные объекты, не имеющих связей - ent_or_rel=0 -  являются вершинами нашего графа
+    sql = "SELECT el.id, el.data  FROM elements as el WHERE el.ent_or_rel=0"
+
+    cursor.execute(sql) # Выполняем sql-запрос
+    nodes = cursor.fetchall() # Получаем массив значений результата sql-запроса
+
+    # В цикле проходимся по каждой строке результата запроса
+    # и добавляем в граф узлы
+    for node in nodes:
+
+        # Вызываем функцию, добавляющую узел в граф, где:
+        # node[0] - id узла;
+        # G - граф;
+        # node[1] - не обязательное поле data, которое мы используем в качестве одного из атрибутов узла;
+        add_node_from_db(node[0], G, node[1])
+
+        # Далее для этого узла ищем дуги и добавляем их в граф:
+        # формируем sql-запрос к таблице relations, описывающей связи между ИО,
+        # и таблице elements, откуда мы получаем поле data для текстового обозначения связи.
+        # Эти связи являются дугами нашего графа.
+        sql = "SELECT rel.id, rel.arg1, rel.arg2, el.data FROM relations as rel, elements as el WHERE rel.id = el.id AND (rel.arg1="+str(node[0])+" OR rel.arg2="+str(node[0])+")"
+
+        cursor.execute(sql) # Выполняем sql-запрос
+        edges = cursor.fetchall() # Получаем массив значений результата sql-запроса
+
+        # Проходимся в цикле по всем строкам результата sql-запроса и добавляем в граф дуги.
+        for edge in edges:
+
+            # Для каждой дуги с помощью отдельной функции получаем словарь атрибутов.
+            edgeAttributes = get_edge_attributes(edge[0])
+
+            # Добавляем в граф дугу с атрибутами id и data,
+            # а также, с полученным отдельно словарем атрибутов - attributes
+            # Возможна ситуация, когда один из узлов дуги ещё не добавлен в граф,
+            # В этом случае, при выполнении функции add_edge() узел будет добавлен автоматически, 
+            # но без необходимых аттрибутов: это исправляется вызовом функции add_node_from_db().
+            G.add_edge(edge[1], edge[2], id=edge[0], data=edge[3], attributes=edgeAttributes)
+            add_node_from_db(int(edge[1]), G) # Добавляем к первому узлу дуги необходимые аттрибуты
+            add_node_from_db(int(edge[2]), G) # Добавляем ко второму узлу дуги необходимые аттрибуты
+
+    # Средствами бибилиотеки NetworkX,
+    # экспортируем граф в виде подходящeм для json-сериализации
+    data = json_graph.node_link_data(G)
+
+    # Создаём экземпляр класса Graph, для хранения структуры графа в базе данных
+    graph = Graph() 
+
+    graph.title = "Semantic" # Определяем заголовок графа
+    graph.body = json.dumps(data) # Преобразуем данные в json-формат
+    graph.save() # Сохраняем граф в собственную базу данных
+
+    return True
+
+
+# Для тестовых целей:
+# Создание графа - многомерной проекции "семантической кучи" - вторым методом
+def create_graph_method_02():
+    print('Create projection using method 02')
+    # Cоздаём пустой NetworkX-граф
+    G = nx.Graph()
+
+    # Устанавливаем соединение с БД, в которой хранятся семантически связанные данные
+    cursor = connections['mysql'].cursor()
+
+    sql = "SELECT rel.id, rel.arg1, rel.arg2, el.data FROM relations as rel, elements as el WHERE rel.id = el.id"
+
+    cursor.execute(sql) # Выполняем sql-запрос
+    edges = cursor.fetchall() # Получаем массив значений результата sql-запроса
+
+    # Проходимся в цикле по всем строкам результата sql-запроса и добавляем в граф дуги.
+    for edge in edges:
+
+        # Для каждой дуги с помощью отдельной функции получаем словарь атрибутов.
+        edgeAttributes = get_edge_attributes(edge[0])
+
+        G.add_edge(edge[1], edge[2], id=edge[0], data=edge[3], attributes=edgeAttributes)
+        add_node_from_db(int(edge[1]), G)
+        add_node_from_db(int(edge[2]), G)
+
+    # Средствами бибилиотеки NetworkX,
+    # экспортируем граф в виде подходящeм для json-сериализации
+    data = json_graph.node_link_data(G)
+
+    # Создаём экземпляр класса Graph, для хранения структуры графа в базе данных
+    graph = Graph() 
+
+    # Определяем заголовок графа
+    graph.title = "Многомерная проекция 'семантической кучи' по заданному фильтру" 
+
+    # Преобразуем данные в json-формат
+    graph.body = json.dumps(data) 
+
+    # Сохраняем граф в собственную базу данных
+    graph.save() 
+
+    return True
+
+
+# Создание графа - многомерной проекции "семантической кучи" - с заданными атрибутами узлов
+def create_filtered_graph(graphFilter):
+    # Cоздаём пустой NetworkX-граф
+    G = nx.Graph()
+
+    # Преобразуем в объект json-массив параметров, полученных из url 
+    try: 
+        graphFilter = json.loads(graphFilter)
+        print_json(graphFilter)
+    except:
+        render_content('Неправильный json-массив graphFilter')
+        raise
+
+    # Обрабатываем массив filterAttributes
+    try:
+        filterAttributes = graphFilter['filterAttributes']
+    except:
+        render_content('Неправильный json-массив filterAttributes')
+        raise
+
+    # Обрабатываем массив filterOptions
+    try:
+        filterOptions = graphFilter['filterOptions']
+        zero = filterOptions['zero']
+    except:
+        render_content('Неправильный json-массив filterOptions')
+        raise
+
+    # Устанавливаем соединение с БД, в которой хранятся семантически связанные данные
+    cursor = connections['mysql'].cursor()
+
+    sql = "SELECT rel.id, rel.arg1, rel.arg2, el.data FROM relations as rel, elements as el WHERE rel.id = el.id"
+
+    cursor.execute(sql) # Выполняем sql-запрос
+    edges = cursor.fetchall() # Получаем массив значений результата sql-запроса
+
+    # Проходимся в цикле по всем строкам результата sql-запроса и добавляем в граф дуги.
+    for edge in edges:
+
+        # Для каждой дуги с помощью отдельной функции получаем словарь атрибутов.
+        edgeAttributes = get_edge_attributes(edge[0])
+
+        G.add_edge(edge[1], edge[2], id=edge[0], data=edge[3], attributes=edgeAttributes)
+        add_node_from_db(int(edge[1]), G)
+        add_node_from_db(int(edge[2]), G)
+
+    # Средствами бибилиотеки NetworkX,
+    # экспортируем граф в виде подходящeм для json-сериализации
+    data = json_graph.node_link_data(G)
+
+    # Создаём экземпляр класса Graph, для хранения структуры графа в базе данных
+    graph = Graph() 
+
+    # Определяем заголовок графа
+    graph.title = "Многомерная проекция 'семантической кучи' по заданному фильтру" 
+
+    # Преобразуем данные в json-формат
+    graph.body = json.dumps(data) 
+
+    # Сохраняем граф в собственную базу данных
+    #graph.save() 
+
+    return graph.body
 
 
