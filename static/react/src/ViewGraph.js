afab564bfaa8b48427e85d9c8d27051adf7ebf07
sergeysynergy <sergeysynergy@gmail.com>
2016-04-14 11:48:18 +0300

    0414

--- static/react/src/ViewGraph.js
+++ static/react/src/ViewGraph.js
@@ -721,7 +721,7 @@ var SVGScene = React.createClass({
                     //_sceneDoubleClick={this.handleSceneClick}
                     //_sceneClicked={this.clicked}
                     //_handleNodeClick={this.props._handleNodeClick}
-                    //_handleNodeTip={this.props._handleNodeTip}
+                    _handleNodeTip={this.props._handleNodeTip}
                     //onMouseClick={this.mouseClick}
                     setNode={this.setNode}
                 />)
@@ -1010,6 +1010,7 @@ var GraphNode = React.createClass({
             scaleY = 1.9
             text.push(
                 <text 
+                    transform='scale(.5)'
                     x={this.state.x}
                     y={this.state.y+15}
                 >
@@ -1028,6 +1029,7 @@ var GraphNode = React.createClass({
                 //draggable='true'
                 //onDragStart={this.dragStart}
                 //onMouseClick={this.props.mouseClick}
+                onMouseOver={this.onMouseOver}
             >
                 <NodeType
                     {...this.props}
@@ -1063,7 +1065,7 @@ var GraphNodePerson = React.createClass({
                 <path
                     transform="scale(.3) translate(-25,-25)"
                     d="M 24.827,0 C 11.138,0 0.001,11.138 0.001,24.827 c 0,13.689 11.137,24.827 24.826,24.827 13.688,0 24.826,-11.138 24.826,-24.827 C 49.653,11.138 38.517,0 24.827,0 Z m 14.315,38.51 c 0,-0.574 0,-0.979 0,-0.979 0,-3.386 -3.912,-4.621 -6.006,-5.517 -0.758,-0.323 -2.187,-1.011 -3.653,-1.728 -0.495,-0.242 -0.941,-0.887 -0.997,-1.438 l -0.162,-1.604 c 1.122,-1.045 2.133,-2.5 2.304,-4.122 l 0.253,0 c 0.398,0 0.773,-0.298 0.832,-0.663 l 0.397,-2.453 c 0.053,-0.524 -0.442,-0.842 -0.843,-0.842 0.011,-0.052 0.02,-0.105 0.025,-0.149 0.051,-0.295 0.082,-0.58 0.102,-0.857 0.025,-0.223 0.045,-0.454 0.056,-0.693 0.042,-1.158 -0.154,-2.171 -0.479,-2.738 -0.33,-0.793 -0.83,-1.563 -1.526,-2.223 -1.939,-1.836 -4.188,-2.551 -6.106,-1.075 -1.306,-0.226 -2.858,0.371 -3.979,1.684 -0.612,0.717 -0.993,1.537 -1.156,2.344 -0.146,0.503 -0.243,1.112 -0.267,1.771 -0.026,0.733 0.046,1.404 0.181,1.947 -0.382,0.024 -0.764,0.338 -0.764,0.833 l 0.396,2.453 c 0.059,0.365 0.434,0.663 0.832,0.663 l 0.227,0 c 0.36,1.754 1.292,3.194 2.323,4.198 l -0.156,1.551 c -0.056,0.55 -0.502,1.193 -0.998,1.438 -1.418,0.692 -2.815,1.358 -3.651,1.703 -1.97,0.812 -6.006,2.131 -6.006,5.517 l 0,0.766 C 7.033,34.756 5.005,30.031 5.005,24.83 c 0,-10.932 8.894,-19.826 19.826,-19.826 10.933,0 19.826,8.894 19.826,19.826 -0.004,5.303 -2.109,10.116 -5.515,13.68 z"/>
-                {/*this.props.text*/}
+                {this.props.text}
             </g>
         )
                 //<circle cx='0' cy='0' r='1' fill={'purple'} />
@@ -1153,7 +1155,7 @@ var GraphNodeEvent = React.createClass({
                     height="8"
                     width="8"
                 />
-                {/*this.props.text*/}
+                {this.props.text}
             </g>
         )
     }
